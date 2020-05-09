fab19db870800d37da4abc3767c1c5fa9f27ceb6
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-05-09 15:36:31 +0530

    Create Lesson GraphQL Mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
-import { Resolver, Query } from "@nestjs/graphql";
+import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
 import { LessonType } from "./lesson.type";
+import { LessonService } from "./lesson.service";
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query(returns => LessonType)
   lesson() {
     return {
@@ -11,5 +14,14 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(
+    @Args("name") name: string,
+    @Args("startDate") startDate: string,
+    @Args("endDate") endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
